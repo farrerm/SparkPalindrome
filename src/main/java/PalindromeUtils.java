44aaf1b4f3aa509168958479ccb3ddbc14bfd653
farrerm <farrerm@gmail.com>
2019-02-27 19:17:34 -0800

    Delete PalindromeUtils.java

--- src/main/java/PalindromeUtils.java
+++
@@ -1,8 +0,0 @@
-public class PalindromeUtils {
-
-    public static boolean isPalindrome(String line) {
-        line = line.replaceAll("\\s", "").toLowerCase();
-        return line.equals(new StringBuilder(line).reverse().toString());
-    }
-
-}
